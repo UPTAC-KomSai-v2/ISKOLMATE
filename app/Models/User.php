c983228efab2bb2cd492f3110167a09cbd1a58b2
Adrian Mayores <raki.rarufu.dev@gmail.com>
2024-12-12 11:56:42 +0800

    add takes and teaches relationship to Section and User models

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,22 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    /**
+     * The sections that belong to the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function takes_sections(): BelongsToMany
+    {
+        return $this->belongsToMany(Section::class, 'takes', 'stu_id', 'sec_id');
+    }
+    /**
+     * The teaches_sections that belong to the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function teaches_sections(): BelongsToMany
+    {
+        return $this->belongsToMany(Section::class, 'teaches', 'ins_id', 'sec_id');
+    }
 }
