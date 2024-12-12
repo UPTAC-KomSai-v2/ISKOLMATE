e2263f3530d104e86922facfec780629938304b6
shirohama <gomezharryleonard@gmail.com>
2024-12-13 07:58:14 +0800

    Update text areas and add form submit button

--- resources/views/announcements2.blade.php
+++ resources/views/announcements2.blade.php
@@ -1,12 +1,20 @@
 <x-dashboard-layout :name="$name" :position="$position">
     <form action="{{ route('announcements.store') }}" method="POST">
-        @crsf
+        @csrf
         <div class="flex flex-col col-span-2 h-full">
-            <textarea class="overflow-y-scroll resize-none border-2 black m-2 table-cell align-middle" cols="15" rows="3" name="" id="" placeholder="TITLE"></textarea>
-            <textarea class="overflow-y-scroll resize-none m-2" cols="50" rows="15"></textarea>
+            <textarea class="overflow-auto resize-none border-2 border-black m-2 p-2 rounded-lg w-full h-12 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-fuchsia-900"
+                name="title"
+                id="title"
+                placeholder="Enter the title"></textarea>
+            <textarea class="overflow-auto resize-none border-2 border-black m-2 p-2 rounded-lg w-full h-40 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-fuchsia-900"
+                name="content"
+                id="content"
+                placeholder="Enter the content"></textarea>
         </div>
         <div class="flex flex-col content-center">
-            <a href='announcements3' class="bg-fuchsia-900 w-80 text-white rounded-3xl flex border-2 hover:shadow-[inset_35px_35px_20px_#181824_,_inset_-35px_-35px_20px_#242434] hover:cursor-pointer border-white m-2 h-20 justify-center items-center md:text-2xl font-bold">Post</a>
+            <button type="submit" class="bg-fuchsia-900 w-80 text-white rounded-3xl flex border-2 hover:shadow-[inset_35px_35px_20px_#181824_,_inset_-35px_-35px_20px_#242434] hover:cursor-pointer border-white m-2 h-20 justify-center items-center md:text-2xl font-bold">
+                Post
+            </button>
             <a href='announcements' class="bg-fuchsia-900 w-80 text-white rounded-3xl flex border-2 hover:shadow-[inset_35px_35px_20px_#181824_,_inset_-35px_-35px_20px_#242434] hover:cursor-pointer border-white m-2 h-20 justify-center items-center md:text-2xl font-bold">Cancel</a>
         </div>
     </form>
