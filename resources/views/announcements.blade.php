edfb79f1ef7a1bbab38964fb1b9456571191affa
Jujusama <jgmacadine@up.edu.ph>
2024-12-13 01:48:05 +0800

    Color Changes

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -1,20 +1,20 @@
 <x-dashboard-layout :name="$name" :position="$position" :back="'/dashboard'">
     <div class="overflow-hidden flex-col  bg-white text-black rounded-3xl flex border-2  border-white m-2 h-80 ml-0  md:text-2xl font-bold">
-        <div class="text-left bg-[blue] text-[white] p-[10pt]">Admin Announcements</div></a>
+        <div class="text-left bg-[#8D1436] text-[white] p-[10pt]">Admin Announcements</div></a>
         <div class="border-2 p-2">Announcement #1</div>
         <div class="border-2 p-2">Announcement #2</div>
         <div class="border-2 p-2">Announcement #3</div>
         <div class="border-2 p-2">Announcement #4</div>
     </div>
     <div class="overflow-hidden flex-col bg-white text-black rounded-3xl flex border-2  border-white m-3 h-80 ml-0 md:text-2xl font-bold">
-        <div class= "text-left bg-[blue] text-[white] p-[10pt]">Student Announcements</div></a>
+        <div class= "text-left bg-[#8D1436] text-[white] p-[10pt]">Student Announcements</div></a>
         <div class="border-2 p-2">Announcement #1</div>
         <div class="border-2 p-2">Announcement #2</div>
         <div class="border-2 p-2">Announcement #3</div>
         <div class="border-2 p-2">Announcement #4</div>
     </div>
     <div class="overflow-hidden flex-col bg-white text-black rounded-3xl flex border-2  border-white m-3 h-80 ml-0 md:text-2xl font-bold">
-        <div class= "text-left bg-[blue] text-[white] p-[10pt]">Course Specific Announcements</div></a>
+        <div class= "text-left bg-[#8D1436] text-[white] p-[10pt]">Course Specific Announcements</div></a>
         <div class="border-2 p-2">Announcement #1</div>
         <div class="border-2 p-2">Announcement #2</div>
         <div class="border-2 p-2">Announcement #3</div>
--- resources/views/input_tasks.blade.php
+++ resources/views/input_tasks.blade.php
@@ -7,24 +7,24 @@
     <div class="flex-col border-solid border-[#f3e7e7] items-center p-10 rounded-lg">
         <div class="flex mb-5"> 
             <form name="form" action="" method="post">
-                <input name="subject" id="subject" type="text" class="w-[300px] p-2 rounded-md border border-solid border-[#ccc]" placeholder="Subject" required />
+                <input name="subject" id="subject" type="text" class="w-[300px] p-2 rounded-md border border-solid border-[#ccc] text-[#505050]" placeholder="Subject" required />
             </form>
         </div>
         <div class="flex">
-            <label for="fileInput" class="p-[5px] bg-[#f0f0f0] border border-solid border-[#ccc] rounded-l-md cursor-pointer">Upload</label>
+            <label for="fileInput" class="p-[5px] bg-[#00563F] border border-solid border-[#ccc] rounded-l-md cursor-pointer hover:text-[#FEB71C] hover:bg-[#00563F] hover:duration-500 hover:bg-gradient-to-tr from-slate-800 to-slate-950">Upload</label>
             <span class="grow text-center text-[#888] bg-[#f0f0f0] p-[5px] border-x-[none] border-solid border-[#ccc]">Choose file</span>
-            <button class="bg-[#f0f0f0] border cursor-pointer px-2.5 py-[5px] rounded-r-md border-solid border-[#ccc]">Browse</button>
+            <button class="bg-[#00563F] border cursor-pointer px-2.5 py-[5px] rounded-r-md border-solid border-[#ccc] hover:text-[#FEB71C] hover:bg-[#00563F] hover:duration-500 hover:bg-gradient-to-tr from-slate-800 to-slate-950">Browse</button>
         </div>
         <div class="flex justify-between items-center">
             <div class="w-3/4">
                 <label for="description" class="form-label">Description</label>
-                <textarea name="description" id="description" class="form-control w-full rounded-md border border-solid border-[#ccc]" rows="5" placeholder="Enter details here" required></textarea>
+                <textarea name="description" id="description" class="form-control w-full rounded-md border border-solid border-[#ccc] text-[#505050]" rows="5" placeholder="Enter details here" required></textarea>
             </div>
             <div class="flex flex-col justify-center items-end w-1/4">
-                <button class="bg-[#f0f0f0] border cursor-pointer px-4 py-2 w-32 rounded border-solid border-[#ccc] mb-4" onclick="location.href='input_tasks1'">
+                <button class="bg-[#8D1436] border cursor-pointer px-4 py-2 w-32 rounded border-solid border-[#ccc] mb-4 hover:border-[#FEB71C] hover:text-[#FEB71C] hover:font-bold hover:duration-500 hover:bg-gradient-to-tr from-slate-800 to-slate-950" onclick="location.href='input_tasks1'">
                     Confirm
                 </button>
-                <button class="bg-[#f0f0f0] border cursor-pointer px-4 py-2 w-32 rounded border-solid border-[#ccc]" onclick="location.href='tasks'">
+                <button class="bg-[#8D1436] border cursor-pointer px-4 py-2 w-32 rounded border-solid border-[#ccc] hover:border-[#FEB71C] hover:text-[#FEB71C] hover:font-bold hover:duration-500 hover:bg-gradient-to-tr from-slate-800 to-slate-950" onclick="location.href='tasks'">
                     Cancel
                 </button>
             </div>
