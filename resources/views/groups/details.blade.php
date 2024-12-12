b793b2b653e317c825ea74cc15fe42fb854ec66e
shirohama <gomezharryleonard@gmail.com>
2024-12-13 05:29:21 +0800

    Update details.blade to show member list

--- resources/views/groups/details.blade.php
+++ resources/views/groups/details.blade.php
@@ -1,3 +1,11 @@
 <x-dashboard-no-time-layout :back="'/dashboard'">
-    <h1>TEST</h1>
+    <h1>{{ $group->group_name }}</h1>    
+
+    <h2>Group members:</h2>
+
+    <ul>
+        @foreach ($members as $member)
+            <li>{{ $member->name }}</li>
+        @endforeach
+    </ul>
 </x-dashboard-no-time-layout>
