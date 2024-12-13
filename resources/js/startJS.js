99fd7b49c7ad2946e49d65d328f46d9c32855eeb
unknown <examp23.lo23@gmail.com>
2024-12-13 11:31:50 +0800

    Add start page

--- resources/js/startJS.js
+++ resources/js/startJS.js
@@ -1,7 +1,15 @@
 
-redirect();
+setInterval(() => {
+  // location.href = "/start2";
+  redirect();
+  alert("NADARA AKO");
+}, 3000);
+
+// redirect();
 
 function redirect() {
+  alert("NADARA AKO");
+
   setInterval(() => {
     location.href = "/start2";
   }, 3000);
