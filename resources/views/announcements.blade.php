71612a23a2a2d46ef7d5e51fc6c0a075382e268c
Snorcol <ccolasito@up.edu.ph>
2024-12-13 12:10:23 +0800

    Delete Fix
    Curly Brace, all announcement view

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -4,7 +4,7 @@
         class="overflow-hidden flex-col  bg-white text-black rounded-3xl flex border-2  border-white m-2 h-80 ml-0  md:text-2xl font-bold">
         <div class="text-left bg-[#8D1436] text-[white] p-[10pt]">Admin Announcements</div>
         @foreach($announcements as $announcement)
-            <div class="border-2 p-2">{{$announcement->title}}}</div>
+            <div class="border-2 p-2">{{$announcement->title}}</div>
             @php
                 $isCreator = AnnouncementCreator::where('annc_id', $announcement->id)->where('u_id', Auth::id())->exists();
             @endphp
@@ -20,22 +20,32 @@
         class="overflow-hidden flex-col  bg-white text-black rounded-3xl flex border-2  border-white m-2 h-80 ml-0  md:text-2xl font-bold">
         <div class="text-left bg-[#8D1436] text-[white] p-[10pt]">Student Announcements</div>
         @foreach($announcements as $announcement)
-            <div class="border-2 p-2">{{$announcement->title}}}</div>
-
+            <div class="border-2 p-2">{{$announcement->title}}</div>
             @php
                 $isCreator = AnnouncementCreator::where('annc_id', $announcement->id)->where('u_id', Auth::id())->exists();
             @endphp
+            @if($isCreator)
+                <form action="{{route('announcement.destroy', [$announcement->id]) }}" method="POST" onsubmit="return confirm('Are you sure you wish to delete this announcement?');">
+                    @csrf @method('DELETE')
+                    <button type="submit" class="px-4 py-2 rounded-md bg-[#1e1e2f] text-white shadow-[10px_10px_20px_#141418,-10px_-10px_20px_#282838]"> DELETE </button>
+                </form>
+            @endif
         @endforeach
     </div>
     <div
         class="overflow-hidden flex-col  bg-white text-black rounded-3xl flex border-2  border-white m-2 h-80 ml-0  md:text-2xl font-bold">
         <div class="text-left bg-[#8D1436] text-[white] p-[10pt]">Course Specific Announcements</div>
         @foreach($announcements as $announcement)
-            <div class="border-2 p-2">{{$announcement->title}}}</div>
-
+            <div class="border-2 p-2">{{$announcement->title}}</div>
             @php
                 $isCreator = AnnouncementCreator::where('annc_id', $announcement->id)->where('u_id', Auth::id())->exists();
             @endphp
+            @if($isCreator)
+                <form action="{{route('announcement.destroy', [$announcement->id]) }}" method="POST" onsubmit="return confirm('Are you sure you wish to delete this announcement?');">
+                    @csrf @method('DELETE')
+                    <button type="submit" class="px-4 py-2 rounded-md bg-[#1e1e2f] text-white shadow-[10px_10px_20px_#141418,-10px_-10px_20px_#282838]"> DELETE </button>
+                </form>
+            @endif
         @endforeach
     </div>
     <div
