9c488dd34f036e0797c62a781b727e9c7fcafc42
Snorcol <ccolasito@up.edu.ph>
2024-12-13 12:08:35 +0800

    Delete fix
    fix announcement entry deletion

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -9,9 +9,10 @@
                 $isCreator = AnnouncementCreator::where('annc_id', $announcement->id)->where('u_id', Auth::id())->exists();
             @endphp
             @if($isCreator)
-                <form action="{{route('announcement.destroy', [$isCreator => $announcement->id]) }}" method="POST" onsubmit="return confirm('Are you sure you wish to delete this announcement?');">
+                <form action="{{route('announcement.destroy', [$announcement->id]) }}" method="POST" onsubmit="return confirm('Are you sure you wish to delete this announcement?');">
                     @csrf @method('DELETE')
                     <button type="submit" class="px-4 py-2 rounded-md bg-[#1e1e2f] text-white shadow-[10px_10px_20px_#141418,-10px_-10px_20px_#282838]"> DELETE </button>
+                </form>
             @endif
         @endforeach
     </div>
