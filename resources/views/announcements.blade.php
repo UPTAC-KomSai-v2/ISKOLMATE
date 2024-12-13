954d7e8e1f132c53be7860bf9d98dbf1f1f8de7e
Snorcol <ccolasito@up.edu.ph>
2024-12-13 11:34:39 +0800

    clean

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -3,10 +3,8 @@
     <div
         class="overflow-hidden flex-col  bg-white text-black rounded-3xl flex border-2  border-white m-2 h-80 ml-0  md:text-2xl font-bold">
         <div class="text-left bg-[#8D1436] text-[white] p-[10pt]">Admin Announcements</div>
-        </a>
         @foreach($announcements as $announcement)
             <div class="border-2 p-2">{{$announcement->title}}}</div>
-
             @php
                 $isCreator = AnnouncementCreator::where('annc_id', $announcement->id)->where('u_id', Auth::id())->exists();
             @endphp
@@ -20,7 +18,6 @@
     <div
         class="overflow-hidden flex-col  bg-white text-black rounded-3xl flex border-2  border-white m-2 h-80 ml-0  md:text-2xl font-bold">
         <div class="text-left bg-[#8D1436] text-[white] p-[10pt]">Student Announcements</div>
-        </a>
         @foreach($announcements as $announcement)
             <div class="border-2 p-2">{{$announcement->title}}}</div>
 
@@ -37,7 +34,6 @@
     <div
         class="overflow-hidden flex-col  bg-white text-black rounded-3xl flex border-2  border-white m-2 h-80 ml-0  md:text-2xl font-bold">
         <div class="text-left bg-[#8D1436] text-[white] p-[10pt]">Course Specific Announcements</div>
-        </a>
         @foreach($announcements as $announcement)
             <div class="border-2 p-2">{{$announcement->title}}}</div>
 
@@ -51,7 +47,7 @@
             @endif
         @endforeach
     </div>
-    <div 
+    <div
         class="flex">
         <a href="/announcements2" class="bg-[#8D1436] w-80 text-white rounded-3xl flex border-2 hover:shadow-lg m-2 h-16 justify-center items-center md:text-2xl font-bold">
             Create Announcement
