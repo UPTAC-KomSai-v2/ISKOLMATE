ccdfed207660159370645dea9babc0c069aa649a
Jujusama <jgmacadine@up.edu.ph>
2024-12-16 11:56:57 +0800

    rawr

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -13,7 +13,8 @@
             @if($isCreator)
                 <form class="relative" action="{{route('announcement.destroy', [$announcement->id]) }}" method="POST" onsubmit="return confirm('Are you sure you wish to delete this announcement?');">
                     @csrf @method('DELETE')
-                    <button type="submit" class="absolute right-2.5 top-[-3.5rem] px-4 py-2 rounded-md bg-[#1e1e2f] text-white"> DELETE </button>
+                    <button type="submit" class="absolute right-2.5 top-[-3.5rem] px-4 py-2 rounded-md bg-[#1e1e2f] text-white
+                    hover:shadow-lg m-2 h-10 flex justify-center items-center font-bold hover:bg-gradient-to-tr from-slate-800 to-slate-950 transition duration-500 hover:text-[#FEB71C] hover:border-[#FEB71C]"> DELETE </button>
                 </form>
             @endif
         @endforeach
@@ -21,8 +22,7 @@
     <div
         class="flex">
         <a href="/announcements2" class="bg-[#8D1436] w-80 text-center p-10 text text-white rounded-3xl flex border-2 hover:shadow-lg m-2 h-16 justify-center items-center md:text-2xl font-bold
-        hover:bg-gradient-to-tr from-slate-800 to-slate-950 transition duration-500
-                        hover:text-[#FEB71C] hover:border-[#FEB71C]">
+        hover:bg-gradient-to-tr from-slate-800 to-slate-950 transition duration-500 hover:text-[#FEB71C] hover:border-[#FEB71C]">
             Create Announcement
         </a>
     </div>
