7dd6a6687cce8e3e0682af2b0f82188a692b7eff
Jujusama <jgmacadine@up.edu.ph>
2024-12-16 11:36:31 +0800

    I'm committed

--- resources/views/availability.blade.php
+++ resources/views/availability.blade.php
@@ -2,17 +2,22 @@
     <div class="flex justify-between items-center">
         <div class="w-3/4" >
             <label for="description" class="form-label font-bold">Select Date:</label>
-            <input type="datetime-local" id="birthday" name="birthday" class = "bg-[#83173C] text-[#FEB71B]">
+            <input type="date" id="availability_date" name="availability_date" class = "bg-[#83173C] text-[#FEB71B]">
+            <label for="description" class="form-label font-bold">Select Start Time:</label>
+            <input type="time" id="start_time" name="start_time" class = "bg-[#83173C] text-[#FEB71B]">
+            <label for="description" class="form-label font-bold">Select End Time:</label>
+            <input type="time" id="end_time" name="end_time" class = "bg-[#83173C] text-[#FEB71B]">
+            <button class="bg-[#83173C] text-[#fff] border cursor-pointer px-4 py-2 w-48 rounded border-solid border-[#ccc] mb-4 hover:bg-gradient-to-tr from-slate-800 to-slate-950 transition duration-500
+                        hover:text-[#FEB71C] hover:border-[#FEB71C]" onclick="location.href='input_tasks1'">
+                Set Availability
+            </button>
+            <button class="bg-[#83173C] text-[#fff] border cursor-pointer px-4 py-2 w-48 rounded border-solid border-[#ccc] hover:bg-gradient-to-tr from-slate-800 to-slate-950 transition duration-500
+                        hover:text-[#FEB71C] hover:border-[#FEB71C]" onclick="location.href='tasks'">
+                Remove Availability
+            </button>
         </div>
-    </div>
-    <div class="flex flex-col justify-center items-end w-1/4 font-bold">
-        <button class="bg-[#83173C] text-[#fff] border cursor-pointer px-4 py-2 w-48 rounded border-solid border-[#ccc] mb-4 hover:bg-gradient-to-tr from-slate-800 to-slate-950 transition duration-500
-                    hover:text-[#FEB71C] hover:border-[#FEB71C]" onclick="location.href='input_tasks1'">
-            Set Availability
-        </button>
-        <button class="bg-[#83173C] text-[#fff] border cursor-pointer px-4 py-2 w-48 rounded border-solid border-[#ccc] hover:bg-gradient-to-tr from-slate-800 to-slate-950 transition duration-500
-                    hover:text-[#FEB71C] hover:border-[#FEB71C]" onclick="location.href='tasks'">
-            Remove Availability
-        </button>
+        
+    
     </div>
 </x-dashboard-layout>
+    
