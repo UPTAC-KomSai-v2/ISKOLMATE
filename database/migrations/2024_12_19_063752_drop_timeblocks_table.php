69d249256993e253e6c800a7455f8e0e8da72f35
Adrian Mayores <raki.rarufu.dev@gmail.com>
2024-12-19 14:39:26 +0800

    Drop timeblocks table because it is no longer needed

---
+++ database/migrations/2024_12_19_063752_drop_timeblocks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::dropIfExists('timeblocks');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::create('timeblocks', function (Blueprint $table) {
+            $table->id('timeblock_id');
+            $table->time('time_start', precision:0);
+            $table->time('time_end', precision:0);
+        });
+    }
+};
